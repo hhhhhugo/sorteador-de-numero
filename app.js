bab10f320ad54219589f641a420fcc4a88b7d9ce
Hugo Alonso <hugo_alonsog@hotmail.com>
2026-05-28 14:18:08 -0400

    fazendo o sorteador funcionar

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function sortear (){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteador = [];
+    let numero;
     
+    for (let i = 0; i < quantidade; i++){
+        numero = numeroAleatorio(de, ate);
+        sorteador.push(numero);
+    }
+    alert(sorteador);
 }
 function reiniciar (){
-
+    
 }
-sortear
+
+function numeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
